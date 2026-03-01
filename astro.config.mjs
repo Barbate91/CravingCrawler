0fc23847524afa10f63e7cc33d396fb744c9c2f8
jake.armstrong <jake.armstrong@servicenow.com>
2026-03-01 11:15:37 -0800

    prod adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 import scraper from "./src/integrations/scraper.ts";
 
 export default defineConfig({
   output: "server",
+  adapter: node({ mode: "standalone" }),
   integrations: [scraper()],
 });
