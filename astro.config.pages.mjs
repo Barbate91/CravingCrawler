66ff74c15b37541fa0616830bc9e4de6e7769894
Barbate <barbate@example.com>
2026-05-19 20:45:44 -0700

    improved static deployment option

---
+++ astro.config.pages.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  output: "static",
+  site: "https://barbate91.github.io/CravingCrawler",
+  base: "/CravingCrawler",
+});
